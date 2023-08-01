1b3b0acf406a5a9f5f691f0b666ef49f9744dd0f
Athoy Kanti Ray <114008362+ATHOY43259@users.noreply.github.com>
2023-08-02 00:00:15 +0600

    Add files via upload

---
+++ MVC_LAB/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>This is for html</title>
+</head>
+<body>
+      <a href="Controllers/Registration.php">Registration</a>
+</body>
+</html>
